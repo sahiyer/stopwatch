20cf5d4d5da5d51eae59363f82ed1acd5225c842
Sahil Iyer <sahiyer@deloitte.com.au>
2023-07-20 10:22:58 +1000

    Actioned most STARTERCONF comments.

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
+  title: 'Stopwatch',
   description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
+    'A small stopwatch application.',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  url: '',
 };
